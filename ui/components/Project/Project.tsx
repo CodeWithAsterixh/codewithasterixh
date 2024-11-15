7a953db6e0dc398f7efe110c977260ffdb5b6900
Asterixh <92242499+CodeWithAsterixh@users.noreply.github.com>
2024-11-15 13:37:31 +0000

    datasdata's update

--- ui/components/Project/Project.tsx
+++ ui/components/Project/Project.tsx
@@ -4,11 +4,18 @@ import { ProjectSchema } from "@/public/files/projects";
 import axios, { AxiosResponse } from "axios";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { Card, CardContent, Typography, Chip, Box, Skeleton, Button } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Chip,
+  Box,
+  Skeleton,
+  Button,
+} from "@mui/material";
 import { FiArrowRight, FiZap } from "react-icons/fi";
 import { DiGithub } from "react-icons/di";
 import { AiOutlineCloseCircle } from "react-icons/ai";
-
 
 type Props = {
   p_id: string;
@@ -55,9 +62,12 @@ function Project({ p_id }: Props) {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-[#444D61] to-[#5689C0]">
         <AiOutlineCloseCircle className="text-6xl text-red-600 mb-4" />
-        <Typography variant="h4" className="text-white mb-4">Project not found</Typography>
+        <Typography variant="h4" className="text-white mb-4">
+          Project not found
+        </Typography>
         <Typography variant="body1" className="text-white mb-8">
-          It seems like the project you{`'`}re looking for doesn{`'`}t exist. Please go back to the projects page to explore other projects.
+          It seems like the project you{`'`}re looking for doesn{`'`}t exist.
+          Please go back to the projects page to explore other projects.
         </Typography>
         <Button
           variant="contained"
@@ -89,72 +99,142 @@ function Project({ p_id }: Props) {
       </div>
 
       <CardContent>
-        <Typography variant="h5" className="font-semibold text-lg mb-2 text-white">
+        <Typography
+          variant="h5"
+          className="font-semibold text-lg mb-2 text-white"
+        >
           <FiZap className="inline-block mr-2 text-xl" />
           {project.name}
         </Typography>
 
-        <Typography variant="body2" color="text.secondary" className="mb-2 text-white">
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          className="mb-2 text-white"
+        >
           <strong>Project ID:</strong> {project.p_id}
         </Typography>
 
-        <Typography variant="body2" color="text.secondary" className="mb-2 text-white">
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          className="mb-2 text-white"
+        >
           <strong>Status:</strong> {project.status}
         </Typography>
 
-        <Typography variant="body2" color="text.secondary" className="mb-2 text-white">
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          className="mb-2 text-white"
+        >
           <strong>Current Version:</strong> {project.currentVersion}
         </Typography>
 
-        <Typography variant="body2" color="text.secondary" className="mb-4 text-white">
-          <strong>Start Date:</strong> {new Date(project.startDate).toLocaleDateString()}
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          className="mb-4 text-white"
+        >
+          <strong>Start Date:</strong>{" "}
+          {new Date(project.startDate).toLocaleDateString()}
         </Typography>
 
-        <Typography variant="body2" color="text.secondary" className="mb-4 text-white">
-          <strong>Finish Date:</strong> {new Date(project.finishDate).toLocaleDateString()}
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          className="mb-4 text-white"
+        >
+          <strong>Finish Date:</strong>{" "}
+          {new Date(project.finishDate).toLocaleDateString()}
         </Typography>
 
         <div className="mb-4">
-          <Typography variant="body2" color="text.secondary" className="mb-1 text-white">
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            className="mb-1 text-white"
+          >
             <strong>Technologies:</strong>
           </Typography>
           <Box className="flex flex-wrap gap-2">
             {project.technologies.map((tech, index) => (
-              <Chip key={index} label={tech} variant="outlined" size="small" className="text-white border-white" />
+              <Chip
+                key={index}
+                label={tech}
+                variant="outlined"
+                size="small"
+                className="text-white border-white"
+              />
             ))}
           </Box>
         </div>
 
-        <Typography variant="body2" color="text.secondary" className="mb-2 text-white">
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          className="mb-2 text-white"
+        >
           <strong>Description:</strong> {project.overview.description}
         </Typography>
 
         <div className="mb-4">
-          <Typography variant="body2" color="text.secondary" className="mb-1 text-white">
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            className="mb-1 text-white"
+          >
             <strong>Key Features:</strong>
           </Typography>
           <Box className="flex flex-wrap gap-2">
             {project.overview.keyFeatures.map((feature, index) => (
-              <Chip key={index} label={feature} variant="outlined" size="small" className="text-white border-white" />
+              <Chip
+                key={index}
+                label={feature}
+                variant="outlined"
+                size="small"
+                className="text-white border-white"
+              />
             ))}
           </Box>
         </div>
 
-        <Typography variant="body2" color="text.secondary" className="mb-2 text-white">
-          <strong>Future Goals:</strong> {project.futureGoals.join(", ")}
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          className="mb-2 text-white"
+        >
+          <strong>Future Goals:</strong>{" "}
+          {project.futureGoals.length > 0
+            ? project.futureGoals.join(", ")
+            : "There's currently no future support as the project is currently not maintained."}
         </Typography>
 
         <div className="mb-4">
-          <Typography variant="body2" color="text.secondary" className="mb-1 text-white">
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            className="mb-1 text-white"
+          >
             <strong>Updates:</strong>
           </Typography>
           {project.updates.map((update) => (
             <Box key={update.id} className="border-t border-white pt-2">
-              <Typography variant="body2" color="text.secondary" className="text-white">
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className="text-white"
+              >
                 <strong>Version:</strong> {update.version} -{" "}
-                <span className="italic">{new Date(update.date).toLocaleDateString()}</span>
+                <span className="italic">
+                  {new Date(update.date).toLocaleDateString()}
+                </span>
               </Typography>
-              <Typography variant="body2" color="text.secondary" className="text-white">
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className="text-white"
+              >
                 <strong>Features:</strong> {update.updateFeatures.join(", ")}
               </Typography>
             </Box>
