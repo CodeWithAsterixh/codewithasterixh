d066d6a6f8e01f717b0668ec07f816dfb3bfd67d
Asterixh <92242499+CodeWithAsterixh@users.noreply.github.com>
2025-11-11 20:47:15 +0000

    feat(lib): add type definitions for falsy and truthy values

--- lib/preventEmpty.ts
+++ lib/preventEmpty.ts
@@ -1,5 +1,14 @@
-import { Booleanish } from "@/types/structures";
+export type FalsyValue = false | "" | 0 | 0n | null | undefined;
+export type TruthyValue<T> = Exclude<T, FalsyValue>;
 
+export namespace Booleanish {
+  export type Falsy = FalsyValue;
+  export type Truthy<T> = TruthyValue<T>;
+}
+
+export type TrueObject<T> = {
+  [K in keyof T]: Booleanish.Truthy<T[K]>;
+};
 // validateActions?: (value: T) => {
 //     valid: boolean;
 //     message?: string;
