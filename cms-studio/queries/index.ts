3f4df88dbbc7d59cb7d481423beaf67c3aa8e742
Asterixh <92242499+CodeWithAsterixh@users.noreply.github.com>
2025-12-21 18:06:59 +0100

    Update index.ts

--- cms-studio/queries/index.ts
+++ cms-studio/queries/index.ts
@@ -6,7 +6,7 @@ export const portfolioSectionQuery = `*[_type == "portfolio"]{
       type
     },
     headline,
-    projects[]{
+    "projects":[visibility==true]{
       excerpt,
       slug,
       tags,
