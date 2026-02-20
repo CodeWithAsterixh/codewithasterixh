c7abf77f171c29f22e9587447d4ee94cc5ae7180
CodeWithAsterixh <peterpaultolulope@gmail.com>
2026-02-20 18:54:11 +0100

    refactor: remove remote image configuration and download script
    - Remove Next.js remote image patterns for Sanity CDN as images are now served locally
    - Delete download-images.js script since image fetching is no longer required

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images:{
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-        pathname: "/images/**",
-      },
-    ],
-    formats: ['image/webp', 'image/avif'],
-  },
   compress: true,
   poweredByHeader: false,
   reactCompiler: true,
